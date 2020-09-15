mc_analysis.py added ai' correct computation

mc_analysis.py
@@ -1,10 +1,11 @@
-from util import Transform, Progress, DataFolder
+import util
 
-from typing import Union, Iterable, List
+from typing import Union, Iterable, List, Dict
 # Union[np.ndarray, Iterable, int, float]
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import pickle
 
 
@@ -14,23 +15,222 @@ def main():
     """
     # Path extraction of the MC data
     print('Processing data path.')
-    f = DataFolder(data_folder_name='data_drone2')
+    f = util.DataFolder(data_folder_name='data_drone2')
 
-    for vol_number in [3, 4]:
-        csv_paths = f.get_unique_file(extension='.csv', specific_folder=f.folders['raw'][vol_number])
-        assert len(csv_paths) == 1
-        csv_paths = csv_paths[0]
+    for vol_number in [0, 1, 2, 3, 4]:
+        csv_path = f.get_unique_file_path(extension='.csv', specific_folder=f.folders['raw'][vol_number])
 
         # Extract poses of the drone from the motion capture measures
-        print(f'Importing flight located in {csv_paths}\nCleaning raw data.')
-        m = MCAnalysis(csv_paths)  # Importing and cleaning data
+        print(f'Importing flight located in {csv_path}\nCleaning raw data.')
+        m = MCAnalysis(csv_path)  # Importing and cleaning data
         data = m.get_data()
-        pickle.dump(data, open(f.folders['intermediate'][vol_number] + 'mc_measure.pkl', 'wb'))  # Saving
+        pickle.dump(data.df, open(f.folders['raw_python'][vol_number] + 'mc_measure.pkl', 'wb'))  # Saving
         print('Computing poses from Motion Capture measures.')
         poses = m.get_pose()  # Extracting the pose from markers position
         print('Saving computed poses.')
-        pickle.dump(poses, open(f.folders['intermediate'][vol_number] + 'mc_poses.pkl', 'wb'))  # Saving
+        pickle.dump(poses, open(f.folders['raw_python'][vol_number] + 'mc_poses.pkl', 'wb'))  # Saving
         print('\n\n')
+
+
+class MCAnalysis(object):
+    """
+    MCAnalysis regroup the methods to extract the pose from the motion capture data.
+    """
+
+    def __init__(self, path: str):
+        """
+        Load raw MC data. z is the vertical axis oriented from down to top.
+        :param path: path of the .csv file of mc data.
+        """
+        # Naming columns and removing headers
+        self.columns = ['frame_id', 'sub_frame',
+                        'b1_x', 'b1_y', 'b1_z',
+                        'b2_x', 'b2_y', 'b2_z',
+                        'b3_x', 'b3_y', 'b3_z',
+                        'b4_x', 'b4_y', 'b4_z',
+                        'y1_x', 'y1_y', 'y1_z',
+                        'y2_x', 'y2_y', 'y2_z',
+                        'x2_x', 'x2_y', 'x2_z',
+                        'clapet_inf1_x', 'clapet_inf1_y', 'clapet_inf1_z',
+                        'clapet_inf2_x', 'clapet_inf2_y', 'clapet_inf2_z',
+                        'clapet_sup_2_x', 'clapet_sup_2_y', 'clapet_sup_2_z']
+        self.points_name = ['b1', 'b2', 'b3', 'b4', 'y1', 'y2', 'x2']
+
+        data_raw: pd.DataFrame = pd.read_csv(path, sep=';', header=0, names=self.columns)
+
+        # Drop headers
+        data_raw = data_raw.iloc[4:, :].reset_index(drop=True)
+
+        # Casting types
+        dtypes = {e: ('float64' if i > 1 else 'int64') for i, e in enumerate(self.columns)}
+        data_raw = data_raw.astype(dtypes)
+
+        # Frame id start at 0
+        data_raw[['frame_id']] = data_raw[['frame_id']] - 1
+
+        # Cutting non-usable data
+        filename = path.split('/')[-1]
+        to_cut = {'VolAvecPoubelle03.csv': 5000,
+                  'VolAvecPoubelle04.csv': 4000}
+        if filename in to_cut:
+            data_raw.drop(list(data_raw.index)[to_cut[filename]:], inplace=True)
+
+        # From mm to m
+        for e in self.columns[2:]:
+            data_raw[[e]] = data_raw[[e]] * 1e-3
+
+        self.data = MCData(data=data_raw)
+        self.drone_tf_o = np.zeros((4, 4))  # Will be defined in self.c_reference_frame
+
+    def get_data(self) -> 'MCData':
+        """
+        :return: Cleans imported data.
+        """
+        return self.data
+
+    def object_reference_frame(self) -> Dict[str, np.ndarray]:
+        """
+        Define the reference frame of the drone, C.
+
+        :return: All the measured points_name expressed in C.
+        """
+        # Definition of C the reference frame of the drone.
+        # First it's origin C through the computing of OC. With this definition it is almost the center of mass.
+        oc = np.zeros(3)
+        for i in range(4):
+            oc += self.data.get_point(f'b{i + 1}', 0)
+        oc = oc / 4
+
+        # bi's plane equation
+        bis_eq = util.plane_equation(self.data.get_point('b1', 0),
+                                     self.data.get_point('b2', 0),
+                                     self.data.get_point('b3', 0))
+        plane_normal = bis_eq[0:3]
+
+        # x axis definition
+        b1 = self.data.get_point('b1', 0)
+        y2 = self.data.get_point('y2', 0)
+        y2proj = (y2 - b1) - ((y2 - b1) @ plane_normal) * plane_normal + b1
+        x = (y2proj - oc) / np.linalg.norm(y2proj - oc)
+
+        # z axis definition
+        z = plane_normal / np.linalg.norm(plane_normal)
+
+        # y axis definition in O
+        y = np.cross(z, x) / np.linalg.norm(np.cross(z, x))
+
+        # Compute ai'
+        self.drone_tf_o = util.Transform().from_trans_n_axis(trans=oc, x=x, y=y, z=z)
+        res = {}
+        for point_name in self.points_name:
+            # TODO delete prints
+            # print(f'{point_name} in O : {self.data.get_point(point_name, 0)}')
+            res[point_name] = self.drone_tf_o @ self.data.get_point(point_name, 0)
+            # print(f'{point_name} in drone : {res[point_name]}')
+
+        # TITLE : Plotting ai and ai'
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111, projection='3d')
+        # for i, point_name in enumerate(self.points_name):
+        #     # R0
+        #     point = self.data.get_point(point_name, 0)
+        #     ax.scatter(xs=point[0], ys=point[1], zs=point[2], c='tab:blue')
+        #     ax.text(x=point[0], y=point[1], z=point[2], s=point_name)
+        #     # Drone
+        #     point = res[point_name]
+        #     ax.scatter(xs=point[0], ys=point[1], zs=point[2], c='tab:orange')
+        #     ax.text(x=point[0], y=point[1], z=point[2], s=point_name)
+        #     if i == 0:
+        #         ax.scatter(xs=point[0], ys=point[1], zs=point[2], c='tab:blue', label='R0')
+        #         ax.scatter(xs=point[0], ys=point[1], zs=point[2], c='tab:orange', label='Drone')
+        #
+        # # y2 projected
+        # point = y2proj
+        # point_name = 'y2proj'
+        # ax.scatter(xs=point[0], ys=point[1], zs=point[2], c='tab:green')
+        # ax.text(x=point[0], y=point[1], z=point[2], s=point_name)
+        #
+        # # C
+        # point = oc
+        # point_name = 'C'
+        # ax.scatter(xs=point[0], ys=point[1], zs=point[2], c='tab:green')
+        # ax.text(x=point[0], y=point[1], z=point[2], s=point_name)
+        #
+        # # Plane normal
+        # ax.quiver(oc[0], oc[1], oc[2], plane_normal[0], plane_normal[1], plane_normal[2], length=0.1)
+        #
+        # ax.view_init(azim=0, elev=90)
+        # ax.legend()
+        # plt.show()
+
+        return res
+
+    def get_pose(self) -> List[util.Transform]:
+        """
+        Saves the computed pose from markers position.
+        """
+        figsize = (10, 12)
+        # for e in ['b1', 'b2', 'b3', 'b4', 'x2', 'y1', 'y2']:
+        #     fig, axs = plt.subplots(3, figsize=figsize)
+        #     exec(f"self.self.df.df.plot(y='{e}_x', grid=True, legend=False, title='{e}_x', ax=axs[0])")
+        #     exec(f"self.self.df.df.plot(y='{e}_y', grid=True, legend=False, title='{e}_y', ax=axs[1])")
+        #     exec(f"self.self.df.df.plot(y='{e}_z', grid=True, legend=False, title='{e}_z', ax=axs[2])")
+        #     # plt.savefig(ABSOLUTE_PATH+f'plots/motion_capture_trajectory_comparison/{e}')
+        # plt.show()
+
+        # for axis in ['x', 'y', 'z']:
+        #     fig = plt.figure(figsize=figsize)
+        #     plt.title(axis)
+        #     # xx = np.linspace(0, len(self.df.df) - 1, len(self.df.df))
+        #     end_x = 3000
+        #     xx = np.linspace(0, end_x - 1, end_x)
+        #     for e in ['b1', 'b2', 'b3', 'b4', 'x2', 'y1', 'y2']:
+        #         plt.plot(xx, self.data.df[e + '_' + axis][0:end_x], label=e)
+        #         plt.legend()
+        # plt.show()
+
+        poses = []
+
+        # Compute Transformation between drone and motion capture reference frame for all data
+        # Inspired by :
+        # http://nghiaho.com/?page_id=671&fbclid=IwAR3ss4avz2OyZmGeQRe9ZhDFF5slMKDQa3LLaSGZttcggvzkCqBBjM7MKvA
+
+        # Initialisation
+        ai_mat = np.zeros((len(self.points_name), 3))
+        ai_prime_mat = np.zeros((len(self.points_name), 3))
+        p = util.Progress(len(self.data))
+
+        # Get ai'
+        ai_prime = self.object_reference_frame()
+        for i, point_name in enumerate(self.points_name):
+            ai_prime_mat[i, :] = ai_prime[point_name]
+        # Get ai' centroid
+        centroid_ai_prime = np.mean(ai_prime_mat, axis=0)
+
+        # Compute centroid for ai and then compute rotation matrix R and translation array T
+        for index in self.data.df.index:
+            # Get ai and their centroid
+            for i, point_name in enumerate(self.points_name):
+                ai_mat[i, :] = self.data.get_point(point_name, index)
+            centroid_ai = np.mean(ai_mat, axis=0)
+
+            # Compute rotation matrix
+            # h = np.transpose(ai_mat - centroid_ai) @ (ai_prime_mat - centroid_ai_prime)
+            h = np.transpose(ai_mat) @ ai_prime_mat
+            u, s, v = np.linalg.svd(h)
+            drone_r_origin = v.T @ u.T
+            if np.linalg.det(drone_r_origin) < 0:
+                u, s, v = np.linalg.svd(drone_r_origin)
+                v[:, 2] = -1 * v[:, 2]
+                drone_r_origin = v.T @ u.T
+
+            # Compute translation array
+            drone_t_origin = centroid_ai_prime - drone_r_origin @ centroid_ai
+
+            ai_prime_mat - (np.transpose(drone_r_origin @ np.transpose(ai_mat)) + drone_t_origin)
+            p.update_pgr()
+            pass
+        return poses
 
 
 class MCData(object):
@@ -51,136 +251,42 @@ class MCData(object):
     def __len__(self):
         return len(self.df)
 
-    def get_val(self, columns: List[str], index: Union[np.ndarray, Iterable, int, float]):
+    def get_val(self, columns: List[str], index: Union[np.ndarray, Iterable, int, float]) -> np.ndarray:
+        """
+        Get the values of df given column names and indexes.
+        :param columns: The columns in which the values are. Possible columns are :
+
+        ['frame_id', 'sub_frame',
+         'b1_x', 'b1_y', 'b1_z',
+         'b2_x', 'b2_y', 'b2_z',
+         'b3_x', 'b3_y', 'b3_z',
+         'b4_x', 'b4_y', 'b4_z',
+         'y1_x', 'y1_y', 'y1_z',
+         'y2_x', 'y2_y', 'y2_z',
+         'x2_x', 'x2_y', 'x2_z',
+         'clapet_inf1_x', 'clapet_inf1_y', 'clapet_inf1_z',
+         'clapet_inf2_x', 'clapet_inf2_y', 'clapet_inf2_z',
+         'clapet_sup_2_x', 'clapet_sup_2_y', 'clapet_sup_2_z']
+
+        :param index: The indexes in which the values are.
+        :return: The values in a numpy array.
+        """
         return self.df[columns].iloc[index].to_numpy()
 
-    def get_array(self, point_name: str, index: Union[np.ndarray, Iterable, int, float]):
+    def get_point(self, point_name: str, index: Union[np.ndarray, Iterable, int, float]) -> np.ndarray:
+        """
+        Get the 3 coordinates of the point in self.df giving the point name and it's index in the pd.DataFrame
+        :param point_name: The name of the point. Possible point name are :
+
+        ['b1', 'b2', 'b3', 'b4', 'y1', 'y2', 'x2', 'clapet_inf1', 'clapet_inf2', 'clapet_sup_2']
+
+        :param index: The index in df.
+        :return: The coordinates of the points_name in the form of a numpy array of length 3.
+        """
         columns = [point_name + '_x', point_name + '_y', point_name + '_z']
         for e in columns:
             assert e in self.columns
         return self.df[columns].iloc[index].to_numpy()
-
-    def get_center(self, index: Union[np.ndarray, Iterable, int, float]):
-        return self.df[['y1_x', 'y1_y', 'y2_z']].iloc[index].to_numpy()
-
-
-class MCAnalysis(object):
-    """
-    MCAnalysis regroup the methods to extract the pose from the motion capture data.
-    """
-
-    def __init__(self, path: str):
-        """
-        Load raw MC data. z is the vertical axis oriented from down to top.
-        :param path: path of the .csv file of mc data.
-        """
-        # Naming columns and removing headers
-        columns = ['frame_id', 'sub_frame',
-                   'b1_x', 'b1_y', 'b1_z',
-                   'b2_x', 'b2_y', 'b2_z',
-                   'b3_x', 'b3_y', 'b3_z',
-                   'b4_x', 'b4_y', 'b4_z',
-                   'y1_x', 'y1_y', 'y1_z',
-                   'y2_x', 'y2_y', 'y2_z',
-                   'x2_x', 'x2_y', 'x2_z',
-                   'clapet_inf1_x', 'clapet_inf1_y', 'clapet_inf1_z',
-                   'clapet_inf2_x', 'clapet_inf2_y', 'clapet_inf2_z',
-                   'clapet_sup_2_x', 'clapet_sup_2_y', 'clapet_sup_2_z']
-        data_raw: pd.DataFrame = pd.read_csv(path, sep=';', header=0, names=columns)
-
-        # Drop headers
-        data_raw = data_raw.iloc[4:, :].reset_index(drop=True)
-
-        # Casting types
-        dtypes = {e: ('float64' if i > 1 else 'int64') for i, e in enumerate(columns)}
-        data_raw = data_raw.astype(dtypes)
-
-        # Frame id start at 0
-        data_raw[['frame_id']] = data_raw[['frame_id']] - 1
-
-        # Cutting non-usable data
-        filename = path.split('/')[-1]
-        to_cut = {'VolAvecPoubelle03.csv': 5000,
-                  'VolAvecPoubelle04.csv': 4000}
-        if filename in to_cut:
-            data_raw.drop(list(data_raw.index)[to_cut[filename]:], inplace=True)
-
-        # From mm to m
-        for e in columns[2:]:
-            data_raw[[e]] = data_raw[[e]] * 1e-3
-
-        self.data = MCData(data=data_raw)
-
-    def get_data(self) -> MCData:
-        """
-        :return: Cleans imported data.
-        """
-        return self.data
-
-    def get_pose(self) -> List[Transform]:
-        """
-        Saves the computed pose from markers position.
-        """
-        # figsize = (10, 12)
-        # for e in ['b1', 'b2', 'b3', 'b4', 'x2', 'y1', 'y2']:
-        #     fig, axs = plt.subplots(3, figsize=figsize)
-        #     exec(f"self.self.df.df.plot(y='{e}_x', grid=True, legend=False, title='{e}_x', ax=axs[0])")
-        #     exec(f"self.self.df.df.plot(y='{e}_y', grid=True, legend=False, title='{e}_y', ax=axs[1])")
-        #     exec(f"self.self.df.df.plot(y='{e}_z', grid=True, legend=False, title='{e}_z', ax=axs[2])")
-        #     plt.savefig(ABSOLUTE_PATH+f'plots/motion_capture_trajectory_comparison/{e}')
-        # plt.show()
-
-        # for axis in ['x', 'y', 'z']:
-        #     fig = plt.figure(figsize=figsize)
-        #     plt.title(axis)
-        #     # xx = np.linspace(0, len(self.df.df) - 1, len(self.df.df))
-        #     end_x = 3000
-        #     xx = np.linspace(0, end_x - 1, end_x)
-        #     for e in ['b1', 'b2', 'b3', 'b4', 'x2', 'y1', 'y2']:
-        #         plt.plot(xx, self.data.df[e + '_' + axis][0:end_x], label=e)
-        #         plt.legend()
-        # plt.show()
-
-        # Get reference frame unit axis, compute initial position
-        poses = []
-        index = 0
-        init_position = self.data.get_center(index)
-        tmp = self.data.get_array('y2', index) - init_position
-        x = tmp / np.linalg.norm(tmp)
-        tmp = self.data.get_array('y1', index) - init_position
-        z = tmp / np.linalg.norm(tmp)
-        y = np.cross(z, x)
-
-        # Save Transform object with a translation and an orientation, compute initial orientation
-        init_x, init_y, init_z = x, y, z
-        # Add current_tf_init to saved poses
-        poses.append(Transform().from_euler(init_position, 'xyz', np.zeros(3)))
-        _, init_orientation = poses[0].get_pose()
-
-        init_mat = np.hstack([init_x.reshape((3, 1)), init_y.reshape((3, 1)), init_z.reshape((3, 1))])
-
-        # Compute Transformation for all data
-        p = Progress(len(self.data))
-        for index, row in self.data.df.iterrows():
-            # Get reference frame unit axis, compute actual position
-            index: int
-            position = self.data.get_center(index)
-            tmp = self.data.get_array('y2', index) - position
-            x = tmp / np.linalg.norm(tmp)
-            tmp = self.data.get_array('y1', index) - position
-            z = tmp / np.linalg.norm(tmp)
-            y = np.cross(z, x)
-
-            # Inspired by https://stackoverflow.com/a/34392459/10054528
-            mat = np.hstack([x.reshape((3, 1)), y.reshape((3, 1)), z.reshape((3, 1))])
-            init_d_current = position - init_position
-            init_rot_current = np.transpose(mat @ np.linalg.pinv(init_mat -
-                                                                 np.vstack([init_d_current.reshape((1, 3))
-                                                                            for _ in range(3)])))
-            # Add current_tf_init to saved poses
-            poses.append(Transform().from_rot_matrix_n_trans_vect(init_d_current, init_rot_current))
-            p.update_pgr()
-        return poses
 
 
 if __name__ == '__main__':

posture_error_estimation.py
@@ -1,11 +1,8 @@
 import glob
 import os
+from typing import Union, Iterable, Tuple, List
 import pickle
-import sys
 import time
-import re
-from pathlib import Path
-
 import cv2
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -14,85 +11,54 @@ from cv2 import VideoWriter, VideoWriter_fourcc
 from scipy.spatial.transform import Rotation
 import scipy.integrate as integrate
 
-from util import Progress, Transform, angle_arccos
+from util import Progress, Transform, angle_arccos, DataFolder, \
+    get_file_name, get_folder_path, merge_two_arrays
 
-major, _, _, _, _ = sys.version_info
-assert major == 3
+
+def main():
+    f = DataFolder('data_drone2')
+    # tara preparation
+    tara_left = f.pickle_load_file('.pkl', f.folders['raw_python'][0], 'tara_left')
+    tara_time = []
+    for _, msg in tara_left['tara/left/image_raw'].items():
+        tara_time.append(msg['t'])
+    tara_time = np.array([msg['t'] for key, msg in tara_left['tara/left/image_raw'].items()])
+    tara_time = tara_time - tara_time[0]
+
+    # mc preparation
+    mc_poses = f.pickle_load_file('.pkl', f.folders['raw_python'][0], 'mc_poses')
+    mc_measure = f.pickle_load_file('.pkl', f.folders['raw_python'][0], 'mc_measure')
+    mc_time = np.array(range(len(mc_measure))) / 120
+
+    fig, axs = plt.subplots(3, 1)
+    x_inf = 0
+    x_sup = (len(mc_time) - 1) / 120
+    _, ind_inf = merge_two_arrays(x_inf, mc_time)
+    _, ind_sup = merge_two_arrays(x_sup, mc_time)
+    mc_time_zoomed = mc_time[ind_inf:ind_sup]
+    axs[0].plot(mc_time_zoomed, mc_measure['clapet_sup_2_x'][ind_inf:ind_sup], label='clapet_sup_2_x')
+    axs[0].plot(mc_time_zoomed, mc_measure['clapet_inf2_x'][ind_inf:ind_sup], label='clapet_inf2_x')
+
+    axs[1].plot(mc_time_zoomed, mc_measure['clapet_sup_2_y'][ind_inf:ind_sup], label='clapet_sup_2_y')
+    axs[1].plot(mc_time_zoomed, mc_measure['clapet_inf2_y'][ind_inf:ind_sup], label='clapet_inf2_y')
+
+    axs[2].plot(mc_time_zoomed, mc_measure['clapet_sup_2_z'][ind_inf:ind_sup], label='clapet_sup_2_z')
+    axs[2].plot(mc_time_zoomed, mc_measure['clapet_inf2_z'][ind_inf:ind_sup], label='clapet_inf2_z')
+    plt.show()
+
+    tara_ids, mc_ids = merge_two_arrays(tara_time, )
+
+    f = ErrorEstimation()
+    f.p3_generator()
+    f.p32video()
+
 
 frame_width = 640
 frame_height = 480
 
 
-def aff3d(xyz_array, quat_array, video_path):
-    assert len(xyz_array) == len(quat_array)
-    print(video_path)
-    x_pos = np.array([1, 0, 0])
-    y_pos = np.array([0, 1, 0])
-    z_pos = np.array([0, 0, 1])
-    # minx, miny, minz = np.min(xyz_array, axis=0) - 1
-    # maxx, maxy, maxz = np.max(xyz_array, axis=0) + 1
-
-    width = 640
-    height = 480
-
-    codec = VideoWriter_fourcc(*'H264')
-    fps = 30
-    video = VideoWriter(video_path, codec, float(fps), (width, height), True)
-    prg = Progress(len(xyz_array))
-
-    for xyz, quat in zip(xyz_array, quat_array):
-        ref_tf_pos = Transform().from_pose(xyz, quat).get_inv()
-
-        x_ref = ref_tf_pos.get_rot() @ x_pos
-        y_ref = ref_tf_pos.get_rot() @ y_pos
-        z_ref = ref_tf_pos.get_rot() @ z_pos
-
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-
-        ax.quiver(xyz[0], xyz[1], xyz[2], x_ref[0], x_ref[1], x_ref[2], length=1, normalize=False, color='r')
-        ax.quiver(xyz[0], xyz[1], xyz[2], y_ref[0], y_ref[1], y_ref[2], length=1, normalize=False, color='g')
-        ax.quiver(xyz[0], xyz[1], xyz[2], z_ref[0], z_ref[1], z_ref[2], length=1, normalize=False, color='b')
-
-        ax.scatter(xs=xyz[0] + 1, ys=xyz[1] + 1, zs=xyz[2] + 1, alpha=0)
-        ax.scatter(xs=xyz[0] - 1, ys=xyz[1] - 1, zs=xyz[2] - 1, alpha=0)
-
-        # ax.scatter(xs=minx, ys=miny, zs=minz, alpha=0)
-        # ax.scatter(xs=maxx, ys=maxy, zs=maxz, alpha=0)
-
-        fig.canvas.draw()
-        data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='').reshape((height, width, 3))
-
-        plt.close('all')
-        # plt.show()
-
-        video.write(data[:, :, ::-1])  # Because VideoWriter.write take BGR images
-        prg.update_pgr()
-
-    video.release()
-    cv2.destroyAllWindows()
-    print()
-
-
 class ErrorEstimation(object):
-    def __init__(self, flight_number):
-        self.tic = time.time()
-        self.flight_number = flight_number
-        # Import data
-        flight_files = []
-        i = 0
-        for day in sorted(next(os.walk(ABSOLUTE_PATH))[1]):
-            temp_path = ABSOLUTE_PATH + day + '/'
-            for flight_name in sorted(next(os.walk(temp_path))[1]):
-                flight_path = temp_path + flight_name + '/clean_data/'
-                if i == self.flight_number:
-                    self.flight_name = day + '/' + flight_name + '/'
-                    for filepath in sorted(glob.glob(flight_path + '*.pkl')):
-                        flight_files.append(filepath)
-                    flight_files = sorted(flight_files)
-                i += 1
-
-        self.data = pickle.load(open(flight_files[0], 'rb'))
+    def __init__(self, cameras, poses):
 
         self.time = self.data['time']
         self.xyz = self.data['xyz']
@@ -117,12 +83,6 @@ class ErrorEstimation(object):
             drone_tf_enu = Transform().from_pose(pos, quat)
             drone_tf_flu0 = drone_tf_enu @ enu_tf_flu0
             self.xyz[i], self.quaternions[i] = drone_tf_flu0.get_pose()
-
-        self.tac = time.time()
-        print(f'Primary import done | '
-              f'Time since beginning : {self.tac - self.tic:.03f}s | '
-              f'Time since last step : {self.tac - self.tic:.03f}s')
-        self.toc = time.time()
 
     def get_camera_angle_to_horizontal_plane(self, iteration):
         z_camera_in_camera = np.array([0, 0, 1])  # Vector only rotation are important and NOT translations
@@ -207,15 +167,15 @@ class ErrorEstimation(object):
                 p3 = (40, 40)
             p3_list.append(p3)
         self.data['p3'] = p3_list
-        pickle.dump(self.data, open(ABSOLUTE_PATH + self.flight_name + 'clean_data/data.pkl', 'wb'))
+        # pickle.dump(self.data, open(ABSOLUTE_PATH + self.flight_name + 'clean_data/data.pkl', 'wb'))
 
     def p32video(self):
         fps = 10
         codec = VideoWriter_fourcc(*'H264')
         part1, part2, _ = self.flight_name.split('/')
-        name = ABSOLUTE_PATH + 'error_estimation_videos/' + part1 + '_' + part2 + '_error_estimation.mkv'
-        video = VideoWriter(name, codec, float(fps), (self.frames_width, self.frames_height), True)
-        print(name)
+        # name = ABSOLUTE_PATH + 'error_estimation_videos/' + part1 + '_' + part2 + '_error_estimation.mkv'
+        # video = VideoWriter(name, codec, float(fps), (self.frames_width, self.frames_height), True)
+        # print(name)
 
         frames = self.left_frames
         prg = Progress(len(frames))
@@ -238,39 +198,83 @@ class ErrorEstimation(object):
                       f'Rot x : {angles[0]:8.3f}°\n'
                       f'Rot y : {angles[1]:8.3f}°\n'
                       f'Rot z : {angles[2]:8.3f}°')
-            plt.annotate(legend, (450, 335), xycoords='axes points', size=12, ha='right', va='top',
+            plt.annotate(legend, (450, 335), xycoords='axes points_name', size=12, ha='right', va='top',
                          bbox=dict(boxstyle='round', alpha=0.5, fc='w'))
             ax.axis('off')
             fig.canvas.draw()
             data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='').reshape(shape)
             plt.close('all')
 
-            video.write(data[:, :, ::-1])  # Because VideoWriter.write take BGR images
+            # video.write(data[:, :, ::-1])  # Because VideoWriter.write take BGR images
             prg.update_pgr()
 
-        video.release()
+        # video.release()
         cv2.destroyAllWindows()
         # del video
 
 
+def aff3d(xyz_array, quat_array, video_path):
+    assert len(xyz_array) == len(quat_array)
+    print(video_path)
+    x_pos = np.array([1, 0, 0])
+    y_pos = np.array([0, 1, 0])
+    z_pos = np.array([0, 0, 1])
+    # minx, miny, minz = np.min(xyz_array, axis=0) - 1
+    # maxx, maxy, maxz = np.max(xyz_array, axis=0) + 1
+
+    width = 640
+    height = 480
+
+    codec = VideoWriter_fourcc(*'H264')
+    fps = 30
+    video = VideoWriter(video_path, codec, float(fps), (width, height), True)
+    prg = Progress(len(xyz_array))
+
+    for xyz, quat in zip(xyz_array, quat_array):
+        ref_tf_pos = Transform().from_pose(xyz, quat).get_inv()
+
+        x_ref = ref_tf_pos.get_rot() @ x_pos
+        y_ref = ref_tf_pos.get_rot() @ y_pos
+        z_ref = ref_tf_pos.get_rot() @ z_pos
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+
+        ax.quiver(xyz[0], xyz[1], xyz[2], x_ref[0], x_ref[1], x_ref[2], length=1, normalize=False, color='r')
+        ax.quiver(xyz[0], xyz[1], xyz[2], y_ref[0], y_ref[1], y_ref[2], length=1, normalize=False, color='g')
+        ax.quiver(xyz[0], xyz[1], xyz[2], z_ref[0], z_ref[1], z_ref[2], length=1, normalize=False, color='b')
+
+        ax.scatter(xs=xyz[0] + 1, ys=xyz[1] + 1, zs=xyz[2] + 1, alpha=0)
+        ax.scatter(xs=xyz[0] - 1, ys=xyz[1] - 1, zs=xyz[2] - 1, alpha=0)
+
+        # ax.scatter(xs=minx, ys=miny, zs=minz, alpha=0)
+        # ax.scatter(xs=maxx, ys=maxy, zs=maxz, alpha=0)
+
+        fig.canvas.draw()
+        data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='').reshape((height, width, 3))
+
+        plt.close('all')
+        # plt.show()
+
+        video.write(data[:, :, ::-1])  # Because VideoWriter.write take BGR images
+        prg.update_pgr()
+
+    video.release()
+    cv2.destroyAllWindows()
+    print()
+
+
+def keep_tara_left():
+    f = DataFolder('data_drone2')
+    tara_file = f.pickle_load_file('.pkl', f.folders['raw_python'][0], 'tara__', pickle_was_python2=True)
+    file_location = f.get_unique_file_path('.pkl', f.folders['raw_python'][0], 'tara__')
+
+    tara_left_saving_path = get_folder_path(file_location) + 'tara_left__2020-07-17-15-07-11.pkl'
+    tara_left = tara_file.copy()
+    tara_left.pop('tara/right/image_raw', None)
+    tara_left.pop('tara/right/camera_info', None)
+    pickle.dump(tara_left, open(tara_left_saving_path, 'wb'))
+
+
 if __name__ == '__main__':
-    def main():
-        # pose_source possibilities : ['tubex_estimator, 'board_imu', 'mavros_imu']
-        e = DataPreparation(flight_number=1, pose_source='mavros_imu')
-        e.fusion()
-        f = ErrorEstimation(flight_number=1)
-        f.p3_generator()
-        f.p32video()
-
-        # for flight_number in range(0, 12):
-        #     print(flight_number)
-        #     if flight_number == 1:
-        #         continue
-        #     e = DataPreparation(flight_number=flight_number, pose_source='mavros_imu')
-        #     e.fusion()
-        #     f = ErrorEstimation(flight_number=flight_number)
-        #     f.p3_generator()
-        #     f.p32video()
-
-
     main()

rosbag2python.py
@@ -9,12 +9,15 @@ import pickle
 import time
 from scipy.spatial.transform import Rotation
 
+# This code must be executed with python 2.7 because of the rosbag package.
 major, _, _, _, _ = sys.version_info
 assert major == 2
 
 
-# This code must be executed with python 2.7 because of the rosbag package.
 def main():
+    """
+    Load rosbag file and save (pickle dump) it in python form. Adaptable to any message type.
+    """
     # Data to extract parameters
     abs_path = '/Users/quentin/phd/turbulence/'
     data_folder = 'data_drone2'
@@ -49,7 +52,7 @@ def main():
 
     # Create saving path
     filename = file_to_extract.split('/')[-1].split('.')[0]
-    saving_path = abs_path + data_folder + '/intermediate/' + flights[flight_number] + '/' + filename + '.pkl'
+    saving_path = abs_path + data_folder + '/raw_python/' + flights[flight_number] + '/' + filename + '.pkl'
 
     # Extract data
     e = Extractor(file_to_extract)
@@ -102,97 +105,6 @@ class Extractor(object):
                         print()
                     topic_msg_analysed.append(topic)
             bag.close()
-
-    def tara_extract(self, get_topics_types=False, topic='', nb_msg=1):
-        """
-        Extract topic or convert data
-        :param get_topics_types: if get_topics_types=False converts .bag data to .npy format for tara bags.
-        Else print topics and types of topics for all the bag found.
-        :param topic: The topic to extract if get_topics_types=false
-        :param nb_msg: Number of message to display per topic/
-        :return: .npy file for all flights or a print
-
-        Known topics : 'tara/left/image_raw', 'tara/right/image_raw', 'tara/right/camera_info', 'tara/left/camera_info'
-
-        By ros convention, 'tara/right/camera_info' contain the information of the stereo camera which seems to be
-        constant. 'tara/left/camera_info' is empty.
-        """
-        bags = [e for e in self.bags_n_flights if re.search('.*(/tara.*)', e[0]) is not None]
-        # List of tuple (path to tara.bag file, path to tara.bag folder)
-
-        if get_topics_types:
-            self._print_topics_types([e[0] for e in bags], nb_msg)
-        else:
-            assert topic != '', 'Please insert a valid topic (see docstring)'
-            for paths in bags:
-                bag_file_path = paths[0]
-                # bag_folder_path = paths[1]
-                # break
-
-                print(bag_file_path)
-
-                bag = rosbag.Bag(bag_file_path)
-                msg_n = bag.get_message_count(topic)
-                p = Progress(max_iter=msg_n, end_print='\n')
-
-                if topic in ['tara/left/image_raw', 'tara/right/image_raw']:
-                    bag_data = []
-                    time = np.zeros(msg_n)
-                    for i, (topic, msg_raw, t) in enumerate(bag.read_messages(topics=topic)):
-                        time[i] = msg_raw.header.stamp.secs + msg_raw.header.stamp.nsecs * 1e-9
-                        # Shaping data as a ndarray
-                        msg_encoded = repr(msg_raw)
-                        match = re.search('data: .(.*).', msg_encoded)
-                        data_string = match.group(1)
-                        data = np.fromstring(data_string, dtype=np.uint8, sep=', ')
-
-                        # Size of the data
-                        img = data.reshape((self.tara_frame_height, self.tara_frame_width))
-                        bag_data.append(img)
-
-                        p.update_pgr()
-                    array = np.stack(bag_data)
-                    path = bag_file_path[:-4]
-
-                    path_without_filename = path[:-25]
-                    end_filename = path[-21:]
-
-                    if topic == 'tara/right/image_raw':
-                        np.save(path_without_filename + 'tara_right_' + end_filename + '.npy', array)
-                        np.save(path_without_filename + 'tara_right_' + 'time' + end_filename + '.npy', time)
-                    if topic == 'tara/left/image_raw':
-                        np.save(path_without_filename + 'tara_left_' + end_filename + '.npy', array)
-                        np.save(path_without_filename + 'tara_left_' + 'time' + end_filename + '.npy', time)
-
-                elif topic in ['tara/right/camera_info', 'tara/left/camera_info']:
-                    camera_info = {}
-                    for i, (topic, msg_raw, t) in enumerate(bag.read_messages(topics=topic)):
-                        a = np.array(msg_raw.D)
-                        b = np.array(msg_raw.K)
-                        c = np.array(msg_raw.P)
-                        d = np.array(msg_raw.R)
-
-                        if ((not a.shape == (0,) and not np.sum(np.abs(b)) == 0) and
-                                (not np.sum(np.abs(c)) == 0 and not np.sum(np.abs(d)) == 0)):
-                            camera_info['D'] = np.array(msg_raw.D)
-                            camera_info['K'] = np.array(msg_raw.K).reshape((3, 3))
-                            camera_info['P'] = np.array(msg_raw.P).reshape((3, 4))
-                            camera_info['R'] = np.array(msg_raw.R).reshape((3, 3))
-                            break
-                    path = bag_file_path[:-4]
-                    path_without_filename = path[:-25]
-                    end_filename = path[-21:]
-
-                    assert camera_info != {}
-
-                    if topic == 'tara/right/camera_info':
-                        pickle.dump(camera_info, open(path_without_filename + 'tara_info_right_'
-                                                      + end_filename + '.pkl', 'wb'))
-                    if topic == 'tara/left/camera_info':
-                        pickle.dump(camera_info, open(path_without_filename + 'tara_info_left_'
-                                                      + end_filename + '.pkl', 'wb'))
-
-                bag.close()
 
     def tf_extract(self, get_topics_types=False, topic='', nb_msg=1):
         """
@@ -597,12 +509,21 @@ def object_analysis(obj):
 
 class Progress(object):
     def __init__(self, max_iter, end_print=''):
+        """
+        Easy way to progress print a for loop for example.
+        :param max_iter: Maximum iteration of the loop.
+        :param end_print: String that will be printed at the end of the file.
+        """
         self.max_iter = float(max_iter)
         self.end_print = end_print
         self.iter = float(0)
         self.initial_time = time.time()
 
     def update_pgr(self, iteration=None):
+        """
+        Update the print.
+        :param iteration: The actual iteration if there has been more iteration passed than 1.
+        """
         if iteration is not None:
             self.iter = iteration
         # Progression

util.py
@@ -2,12 +2,13 @@ from __future__ import print_function
 
 import numpy as np
 from scipy.spatial.transform import Rotation
-from typing import Union, Tuple, Optional, List
+from typing import Union, Tuple, Optional, List, Iterable
 import sys
 import os
 import re
 import pathlib
 import time
+import pickle
 
 ABSOLUTE_PATH = '/Users/quentin/phd/turbulence/'
 
@@ -47,8 +48,11 @@ class Transform(object):
     def __matmul__(self, multiple: Union['Transform', np.ndarray]) -> Union['Transform', np.ndarray]:
         if type(multiple) is Transform:
             return Transform().from_matrix(self.matrix @ multiple.get_matrix())
-        else:
-            return self.matrix @ multiple
+        else:  # type(mutiple) np.ndarray
+            if multiple.shape == (4,):
+                return self.matrix @ multiple
+            elif multiple.shape == (3,):
+                return (self.matrix @ np.append(multiple, 1))[:3]
 
     def __repr__(self):
         return self.matrix.__repr__()
@@ -92,7 +96,7 @@ class Transform(object):
 
         :return: trans, quat.
 
-        Note: only work for 3D points, P_A = (x_A, y_A, z_A, 1) while applying '@' operator.
+        Note: only work for 3D points_name, P_A = (x_A, y_A, z_A, 1) while applying '@' operator.
 
         C reference frame.
 
@@ -128,7 +132,7 @@ class Transform(object):
         :param quat: quaternions.
         :return: Transform object.
 
-        Note: only work for 3D points, P_A = (x_A, y_A, z_A, 1) while applying '@' operator.
+        Note: only work for 3D points_name, P_A = (x_A, y_A, z_A, 1) while applying '@' operator.
 
         C reference frame.
 
@@ -181,7 +185,7 @@ class Transform(object):
         :return: B_T_A = tf = Transform object such as
         P_B = Transform().from_rot_matrix_n_trans_vect(trans, quat) @ P_A.
 
-        Note: only work for 3D points, P_A = (x_A, y_A, z_A, 1) while applying '@' operator.
+        Note: only work for 3D points_name, P_A = (x_A, y_A, z_A, 1) while applying '@' operator.
 
         C reference frame.
 
@@ -191,6 +195,28 @@ class Transform(object):
         Return the Transform object from a rotation matrix and a translation vector.
         """
         quat = Rotation.from_matrix(rot).as_quat()
+        self.from_pose(trans, quat)
+        return self
+
+    def from_trans_n_axis(self, trans: np.ndarray, x: np.ndarray, y: np.ndarray, z: np.ndarray) -> 'Transform':
+        """
+
+        :param x: unit vector of B x-axis expressed in A.
+        :param y: unit vector of B y-axis expressed in A.
+        :param z: unit vector of B z-axis expressed in A.
+        :param trans: A_D_B = translation from A origin to B origin described in A.
+        :return: The corresponding Transform object
+        """
+        assert x.shape == (3,)
+        assert y.shape == (3,)
+        assert z.shape == (3,)
+        assert trans.shape == (3,)
+
+        # Inspired by https://stackoverflow.com/a/34392459/10054528
+        a_r_b = np.hstack([x.reshape((3, 1)),  # r = A_R_B
+                           y.reshape((3, 1)),
+                           z.reshape((3, 1))])
+        quat = Rotation.from_matrix(a_r_b).as_quat()
         self.from_pose(trans, quat)
         return self
 
@@ -264,7 +290,7 @@ class DataFolder(object):
             else:
                 f(v)
 
-    def get_files_paths(self, extension: str, specific_folder: Union[str, List[str]] = None,
+    def get_files_paths(self, extension: str = None, specific_folder: Union[str, List[str]] = None,
                         filename_begin_with: str = None) -> List[str]:
         """
         :param extension: The name of the extension.
@@ -273,64 +299,238 @@ class DataFolder(object):
         :return: List of paths of all raw data files that are named with this extension.
         """
         assert extension in self.raw_sorting_dict
-        if specific_folder is None:
-            return self.raw_sorting_dict[extension]
-        else:
+        if extension is None:
             to_keep = []
-            if type(specific_folder) is str:
-                specific_folder = [specific_folder]
-            nominee = self.raw_sorting_dict[extension].copy()
-            for fold in specific_folder:
-                for file_path in nominee:
-                    file_folder = '/'.join(file_path.split('/')[:-1]) + '/'
-                    if file_folder == fold:
-                        to_keep.append(file_path)
-            to_look = to_keep.copy()
-            for file_path in to_look:
-                filename = file_path.split('/')[-1]
-                res = re.search(filename_begin_with, filename)
-                if res is None:
-                    to_keep.remove(file_path)
-            if not to_keep:
-                print('Nothing found.')
-            return to_keep
+            for ext, paths in self.raw_sorting_dict.items():
+                to_keep += paths
+        else:
+            to_keep = self.raw_sorting_dict[extension]
 
-    def get_unique_file(self, extension: str, specific_folder: Union[str, List[str]] = None,
-                        filename_begin_with: str = None) -> List[str]:
-        """
-        :param extension: The name of the extension.
-        :param specific_folder: Path or list of paths to match the file's folder's path.
-        :param filename_begin_with: Beginning of the filename.
-        :return: List of paths of all raw data files that are named with this extension.
-        """
-        assert extension in self.raw_sorting_dict
+        # specific_folder search
+        to_look = to_keep.copy()
         if specific_folder is None:
-            return self.raw_sorting_dict[extension]
+            to_keep = to_look.copy()
         else:
             to_keep = []
             if type(specific_folder) is str:
                 specific_folder = [specific_folder]
-            nominee = self.raw_sorting_dict[extension].copy()
+            nominee = to_look.copy()
             for fold in specific_folder:
                 for file_path in nominee:
-                    file_folder = '/'.join(file_path.split('/')[:-1]) + '/'
+                    file_folder = get_folder_path(file_path)
                     if file_folder == fold:
                         to_keep.append(file_path)
-            to_look = to_keep.copy()
+
+        # filename_begin_with search
+        to_look = to_keep.copy()
+        if filename_begin_with is not None:
             for file_path in to_look:
-                filename = file_path.split('/')[-1]
+                filename = get_file_name(file_path)
                 res = re.search(filename_begin_with, filename)
                 if res is None:
                     to_keep.remove(file_path)
             if not to_keep:
                 print('Nothing found.')
                 raise FileNotFoundError
-            elif len(to_keep) > 1:
-                print('Too much files founded :')
-                print(to_keep)
-            else:
-                return to_keep[0]
+            return to_keep
+        else:
+            return to_keep
 
+    def get_unique_file_path(self, extension: str = None, specific_folder: Union[str, List[str]] = None,
+                             filename_begin_with: str = None) -> str:
+        """
+        :param extension: The name of the extension.
+        :param specific_folder: Path or list of paths to match the file's folder's path.
+        :param filename_begin_with: Beginning of the filename.
+        :return: List of paths of all raw data files that are named with this extension.
+        """
+        to_keep = self.get_files_paths(extension, specific_folder, filename_begin_with)
+        if len(to_keep) > 1:
+            print('Too much files founded :')
+            print(to_keep)
+            raise FileNotFoundError
+        else:
+            return to_keep[0]
+
+    def pickle_load_file(self, extension: str = None, specific_folder: Union[str, List[str]] = None,
+                         filename_begin_with: str = None, pickle_was_python2: bool = False):
+        """
+        :param extension: The name of the extension.
+        :param specific_folder: Path or list of paths to match the file's folder's path.
+        :param filename_begin_with: Beginning of the filename.
+        :param pickle_was_python2: If the file was pickle using python2 or not.
+        :return: List of paths of all raw data files that are named with this extension.
+        """
+
+        to_load = self.get_unique_file_path(extension, specific_folder, filename_begin_with)
+        if pickle_was_python2:
+            return pickle.load(open(to_load, 'rb'), encoding='latin1')
+        else:
+            return pickle.load(open(to_load, 'rb'))
+
+
+def get_file_name(file_path: Union[str, List[str]]) -> Union[str, List[str]]:
+    """
+    Return the files names from the files paths.
+    :param file_path: The paths of the files.
+    :return: The files names.
+    """
+    if type(file_path) == str:
+        assert file_path[-1] != '/', "file_path end with '/'. It is a folder path."
+        return file_path.split('/')[-1]
+    else:  # file_path is a list
+        for e in file_path:
+            assert e[-1] != '/', "file_path end with '/'. It is a folder path."
+        return [e.split('/')[-1] for e in file_path]
+
+
+def get_folder_path(file_path: Union[str, List[str]]) -> Union[str, List[str]]:
+    """
+    Remove the file name in the file path to ge the path of the folder containing the file.
+    :param file_path: The path of the file.
+    :return: Folder path.
+    """
+    if type(file_path) == str:
+        assert file_path[-1] != '/', "file_path end with '/'. It is a folder path."
+        return '/'.join(file_path.split('/')[:-1]) + '/'
+    else:  # file_path is a list
+        for e in file_path:
+            assert e[-1] != '/', "file_path end with '/'. It is a folder path."
+        return ['/'.join(e.split('/')[:-1]) + '/' for e in file_path]
+
+
+def merge_two_arrays(array1: Union[np.ndarray, Iterable, int, float],
+                     array2: Union[np.ndarray, Iterable, int, float]) -> Tuple[List[int], List[int]]:
+    """
+    From two sorted arrays of different length and uniques values : array1 and array2.
+    :param array1: An array of values to be compared to array2.
+    :param array2: An array of values to be compared to array1.
+    :return: Return one indices array for each that indicate the closest value.
+    """
+    is_array1_iterable = is_iterable(array1)
+    is_array2_iterable = is_iterable(array2)
+
+    input_vars = {'array1': array1, 'array2': array2}
+    for name, arr in input_vars.items():
+        if isinstance(arr, int) or isinstance(arr, float):
+            input_vars[name] = np.array([input_vars[name]])
+        elif not isinstance(arr, np.ndarray):
+            input_vars[name] = np.asarray(input_vars[name])
+    array1 = input_vars['array1']
+    array2 = input_vars['array2']
+
+    # Hypothesis verification
+    assert len(array1) > 0, 'array1 must not be empty'
+    assert len(array1) == len(np.unique(array1)), 'array1 must have unique elements.'
+    assert array1.any() == np.sort(array1).any(), 'array1 must be sorted.'
+
+    assert len(array2) > 0, 'array2 must not be empty'
+    assert len(array2) == len(np.unique(array2)), 'array2 must have unique elements.'
+    assert array2.any() == np.sort(array2).any(), 'array2 must be sorted.'
+
+    # Initialisation
+    little_ids = []
+    big_ids = []
+    if len(array1) < len(array2):
+        case = 0
+        little_array = array1.copy()
+        is_little_iterable = is_array1_iterable
+        big_array = array2.copy()
+        is_big_iterable = is_array2_iterable
+    else:
+        case = 1
+        little_array = array2.copy()
+        is_little_iterable = is_array2_iterable
+        big_array = array1.copy()
+        is_big_iterable = is_array1_iterable
+    little_array_remaining_id = {i: val for i, val in enumerate(little_array)}  # The remaining idx not taken
+    big_array_remaining_id = {i: val for i, val in enumerate(big_array)}  # The remaining idx not taken
+
+    # Algorithm begin
+    for little_id, e1 in enumerate(little_array):
+        # Find the closer to e1 in big_array
+        big_id, _ = find_nearest(big_array, e1)
+        if big_id in big_array_remaining_id:
+            little_ids.append(little_id)
+            big_ids.append(big_id)
+            del little_array_remaining_id[little_id]
+            del big_array_remaining_id[big_id]
+        else:
+            diff1 = np.abs(little_array[little_id - 1] - big_array[big_id])
+            diff2 = np.abs(little_array[little_id] - big_array[big_id])
+            if diff1 > diff2:
+                little_ids.append(little_id)
+                big_ids.append(big_id)
+                del little_array_remaining_id[little_id]
+                del big_array_remaining_id[big_id]
+                little_array_remaining_id[little_id - 1] = little_array[little_id - 1]
+                big_array_remaining_id[big_id - 1] = big_array[little_id - 1]
+
+    assert len(little_ids) > 0, 'No correspondence founded'
+    assert len(big_ids) > 0, 'No correspondence founded'
+
+    if (not is_little_iterable) or (not is_big_iterable):
+        little_ids = little_ids[0]
+        big_ids = big_ids[0]
+
+    if case == 0:
+        return little_ids, big_ids
+    else:
+        return big_ids, little_ids
+
+
+def plane_equation(p1: Union[np.ndarray, list], p2: Union[np.ndarray, list],
+                   p3: Union[np.ndarray, list]) -> np.ndarray:
+    """
+    Compute the equation of 2D plane from 3D points_name.
+    :param p1: Point in 3D space different from p2 and p3.
+    :param p2: Point in 3D space different from p1 and p3.
+    :param p3: Point in 3D space different from p2 and p1.
+    :return: The 4 parameters a, b, c, d of a plane equation in an array form.
+    """
+    # Type correction
+    input_vars = {'p1': p1, 'p2': p2, 'p3': p3}
+    for name, arr in input_vars.items():
+        if isinstance(arr, list) or isinstance(arr, float):
+            input_vars[name] = np.array(input_vars[name])
+        elif not isinstance(arr, np.ndarray):
+            raise TypeError('Points type must be list or np.ndarray')
+    p1 = input_vars['p1']
+    p2 = input_vars['p2']
+    p3 = input_vars['p3']
+
+    # Vector P
+    p2_p1 = (p2 - p1) / np.linalg.norm(p2 - p1)
+    p3_p1 = (p3 - p1) / np.linalg.norm(p3 - p1)
+    plane_normal = np.cross(p3_p1, p2_p1)
+    d = - plane_normal @ p1
+    return np.hstack([plane_normal, d])
+
+
+def find_nearest(array, value):
+    """
+    Find nearest index and value into an array according to an input value. Nearest i.e with the smaller absolute value.
+    :param array: The array to be analysed.
+    :param value: The input value.
+    :return: index and value.
+    """
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx, array[idx]
+
+
+def is_iterable(obj) -> bool:
+    """
+    Test if obj is an iterable or not.
+    :param obj: An object to be tested.
+    :return: True or False.
+    """
+    try:
+        iter(obj)
+    except TypeError:
+        return False
+    else:
+        return True
 
 
 def object_analysis(obj):
@@ -365,16 +565,6 @@ def object_analysis(obj):
         except AttributeError:
             print('Attribute : {} of object {} is listed by dir() but cannot be accessed.'.format(e, type(obj)))
     return res
-
-
-def is_list_array_unique(list_arrays):
-    """
-    Compare if two list of numpy arrays are equal element-wise.
-    :param list_arrays: List of numpy array
-    :return: boolean
-    """
-    a = list_arrays[0]
-    return all(np.array_equal(a, x) for x in list_arrays)
 
 
 def angle_arccos(x: np.ndarray, y: np.ndarray) -> float: